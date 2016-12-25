43d103039d59aee97c0e43a38fdf3698629959d0
Sthefany <sthefanyflorianog@gmail.com>
2016-12-24 19:15:11 -0500

    Portfolio update

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,25 @@
 		$(".mid-menu").toggleClass("mid-animate");
 		$(".bottom-menu").toggleClass("bottom-animate");
 	});
+	$(function () {
+		/*
+		- how to call the plugin:
+		$( selector ).cbpQTRotator( [options] );
+		- options:
+		{
+			// default transition speed (ms)
+			speed : 700,
+			// default transition easing
+			easing : 'ease',
+			// rotator interval (ms)
+			interval : 8000
+		}
+		- destroy:
+		$( selector ).cbpQTRotator( 'destroy' );
+		*/
+		$('#cbp-qtrotator').cbpQTRotator();
 
+	});
 	/* ---------------------------------------------- /*
 		 * Home BG
 	/* ---------------------------------------------- */
@@ -93,13 +111,7 @@
 	$(function () {
 		$('img.cambiandoImagen').hover(sourceSwap, sourceSwap);
 	});
-	/* ---------------------------------------------- /*
-	 * Flip Cards
-	/* ---------------------------------------------- */
-	$(".flip").hover(function () {
-		$(this).find(".card").toggleClass("flipped");
-		return false;
-	});
+
 
 	$(document).ready(function () {
 
@@ -175,7 +187,8 @@
 		}, {
 			offset: '80%'
 		});
-
+		/* ---------------------------------------------- /*
+		
 		/* ---------------------------------------------- /*
 		 * WOW cuando esta en Scroll
 		/* ---------------------------------------------- */
@@ -184,6 +197,7 @@
 			mobile: false
 		});
 		wow.init();
+
 		function isValidEmailAddress(emailAddress) {
 			var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
 			return pattern.test(emailAddress);
@@ -193,7 +207,7 @@
 		 * Contact form ajax
 		/* ---------------------------------------------- */
 
-		$('#contact-form').submit(function(e) {
+		$('#contact-form').submit(function (e) {
 
 			e.preventDefault();
 
@@ -201,33 +215,31 @@
 			var c_email = $('#c_email').val();
 			var c_message = $('#c_message ').val();
 			var response = $('#contact-form .ajax-response');
-			
+
 			var formData = {
-				'name'       : c_name,
-				'email'      : c_email,
-				'message'    : c_message
+				'name': c_name,
+				'email': c_email,
+				'message': c_message
 			};
 
-			if (( c_name== '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email) )) {
+			if ((c_name == '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email))) {
 				response.fadeIn(500);
 				response.html('<i class="fa fa-warning"></i> Please fix the errors and try again.');
+			} else {
+				$.ajax({
+					type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
+					url: 'assets/php/contact.php', // the url where we want to POST
+					data: formData, // our data object
+					dataType: 'json', // what type of data do we expect back from the server
+					encode: true,
+					success: function (res) {
+						var ret = $.parseJSON(JSON.stringify(res));
+						response.html(ret.message).fadeIn(500);
+					}
+				});
 			}
-
-			else {
-					 $.ajax({
-							type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-							url         : 'assets/php/contact.php', // the url where we want to POST
-							data        : formData, // our data object
-							dataType    : 'json', // what type of data do we expect back from the server
-							encode      : true,
-							success		: function(res){
-											var ret = $.parseJSON(JSON.stringify(res));
-											response.html(ret.message).fadeIn(500);
-							}
-						});
-				}           
-            	return false;
-			});
+			return false;
+		});
 	});
 
 })(jQuery);
